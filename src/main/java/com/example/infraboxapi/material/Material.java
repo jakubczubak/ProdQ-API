9ca696ac76c135c0df1acc90d807ae89a1fed62e
Kuba <czubakjakub94@gmail.com>
2024-10-29 13:40:17 +0100

    edited Material

--- src/main/java/com/example/infraboxapi/material/Material.java
+++ src/main/java/com/example/infraboxapi/material/Material.java
@@ -24,7 +24,7 @@ public class Material {
     private Integer id;
 
     private BigDecimal pricePerKg;
-    private float minQuantity; // Rzeczywista minimalna ilość na stanie
+    private float minQuantity; // Proponowany minimalny stan ponizej ktorego powinno byc powiadomienie
     private float proposedMinQuantity; // Proponowany minimalny stan na podstawie analizy zużycia
     private float quantity; // Rzeczywista ilość na stanie
 
@@ -46,12 +46,6 @@ public class Material {
     @Column(name = "updated_on")
     private String updatedOn;
 
-    private float previousQuantity; // Przechowuje poprzednią ilość przed aktualizacją
-    private int additionFrequency; // Częstotliwość dodawania materiału
-    private int removalFrequency; // Częstotliwość zdejmowania materiału
-    private int usageFrequency; // Ogólna liczba użyć materiału
-    private ZonedDateTime lastUsedDate; // Data ostatniego użycia materiału
-    private float averageMonthlyUsage; // Średnie miesięczne zużycie materiału
 
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "material_id")
@@ -63,35 +57,6 @@ public class Material {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         updatedOn = now.format(formatter);
 
-        // Sprawdzenie, czy `quantity` uległo zmianie
-        if (this.quantity != this.previousQuantity) {
-            updateUsageStatistics();
-            this.previousQuantity = this.quantity; // Aktualizacja `previousQuantity` do obecnej wartości `quantity`
-        }
     }
 
-    private void updateUsageStatistics() {
-        if (this.quantity > this.previousQuantity) {
-            additionFrequency++;
-        } else if (this.quantity < this.previousQuantity) {
-            removalFrequency++;
-        }
-
-        usageFrequency++;
-        lastUsedDate = ZonedDateTime.now(ZoneId.of("Europe/Warsaw"));
-
-        // Obliczenie średniego miesięcznego zużycia
-        this.averageMonthlyUsage = (removalFrequency > 0) ?
-                (this.minQuantity * removalFrequency) / 12 : 0;
-
-        // Aktualizacja proponowanej minimalnej ilości na stanie
-        updateProposedMinQuantity();
-    }
-
-    private void updateProposedMinQuantity() {
-        if (averageMonthlyUsage > 0) {
-            // Ustalamy proponowany minimalny stan na podstawie średniego miesięcznego zużycia
-            this.proposedMinQuantity = averageMonthlyUsage * 1.2f; // Przykład: 20% zapasu bezpieczeństwa
-        }
-    }
 }
